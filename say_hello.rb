03329e013ed2fd6e49e710b424e14dcd8d09bb8f
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-18 04:18:24 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -2,17 +2,3 @@
 def say_hello (name="Ruby Programmer")
   puts "Hello #{name}!"
 end
-
-def sum(a,b)
-  if a == 1
-    return "Hello, This is #{(a + b)}"
-  else
-    return a - b
-  end
-end
-
-a = 1
-b = 2
-c = sum(a,b)
-
-puts c
